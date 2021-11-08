a6a88abf8a5eb31a9233d7da4ee5381ea827fd81
Riwat Rai <riwatrj@gmail.com>
2021-11-08 11:36:03 +0000

    404 page content added

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?
+  get_header();
+  ?>
+
+  <div class="container">
+    <div class="sub-container">
+      <h1>404 Page Note Found!</h1>
+    </div>
+  </div>
+
+  <?php
+    get_footer();
+    ?>
